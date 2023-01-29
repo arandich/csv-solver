4946443c1509a583768cc0f77a49ad73db5b75dc
BearPadIO <101512663+arandich@users.noreply.github.com>
2023-01-29 17:10:52 +0300

    Project added

--- internal/table/table.go
+++ internal/table/table.go
@@ -16,33 +16,30 @@ func FindAndSolve(table [][]string) [][]string {
 	for i, v := range table[0][1:] {
 		columns[i] = v
 	}
-	// Иттерируемся по строчкам из map rows, делаем проверку на пустое значение, чтобы пропустить столбец индексов строк
+
 	for i, v := range rows {
 		if i == "" {
 			continue
 		}
-		// После отсеивания 1 столбца идем по массиву ячеек
+
 		for i2, v2 := range v {
-			// Проверяем, если первый символ ячейки равен =, то начинаем обрабатывать ячейку
+
 			if string(v2[0]) == "=" {
-				// Проверяем правильность написания выражения
+
 				re := regexp.MustCompile(`^=[a-zA-Z]+?[0-9]+[+\*\/\-][a-zA-Z]+?[0-9]+$`)
 				ok := re.MatchString(v2)
 				if !ok {
 					log.Println("Invalid expression")
 					return nil
 				}
-				// Вытаскиваем из строки обозначения столбцов, математический оператор, индекс строчки
+
 				columnNameArr := regexp.MustCompile("[a-zA-Z]+").FindAllString(v2, 2)
 				operator := regexp.MustCompile("[+\\*\\/\\-]").FindString(v2)
 				rowArr := regexp.MustCompile("[0-9]+").FindAllString(v2, 2)
 
 				var arg1 = ""
 				var arg2 = ""
-				// Обходим map columns и находим там столбец
-				// Записываем в аргумент значение из map rows, в ключ подставляем индекс строчки
-				// Так как дальше идет массив, то указываем индекс элемента, отнимаем 1, так как у нас есть столбец для индексов строчки и нам его учитывать не нужно
-				// i3-1 = столбец, rowArr[int] = индекс строчки
+
 				for i3, v3 := range columns {
 					if columnNameArr[0] == v3 {
 						if _, ok = rows[rowArr[0]]; !ok {
@@ -71,7 +68,7 @@ func FindAndSolve(table [][]string) [][]string {
 					return nil
 				}
 				res := 0
-				// В зависимости от полученного оператора производим вычисление
+
 				switch operator {
 				case "+":
 					res = arg1I + arg2I
